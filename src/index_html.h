a58d1624c75ff1972ab2e3e19e711f5ee80cea99
Markus Hädrich <markus.haedrich@mail.de>
2023-01-15 20:38:18 +0100

    Colours as variables in css

--- src/index_html.h
+++ src/index_html.h
@@ -1,47 +1,39 @@
 #ifndef INDEX_HTML_H
 #define INDEX_HTML_H
-
-/*  
-    RUSSIAN_VIOLETT     #241E4E
-    ROSE_WOOD           #520100
-    MEDIUM_CHAMPAGNE    #EADAA2
-    SUN_GLOW            #FFD046
-    COPPER_RED          #CE6C47
-    CERISE              #DA4167
-    GAINSBORO           #DFDFDF
-    BRONZE              #D58936
-    TEAL_BLUE           #40798C
-    SHAMROCK_GREEN      #399E5A
-    MIDDLE_PURPLE       #D67AB1
-*/
  
 const char INDEX_HTML[] PROGMEM = R"rawliteral(
 
 <!DOCTYPE HTML>
 <html>
+
 <head>
     <meta content="text/html" ; charset="UTF-8" ; http-equiv="content-type">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <title>Setup</title>
     <style>
-        body 
-        {
-            background-color: #241E4E;
-            background-image: radial-gradient(#15112C, #241E4E, #312966);
-            color: #DFDFDF;
+        :root {
+            --RUSSIAN_VIOLETT: #241E4E;
+            --GREEN_COLOR: #399E5A;
+            --RED_COLOR: #CE6C47;
+            --GAINSBORO: #DFDFDF;
+            --TEAL_BLUE: #40798C;
+        }
+
+        body {
+            background-color: var(--RUSSIAN_VIOLETT);
+            /*            background-image: radial-gradient(#15112C,  #1c1c1c, #312966);*/
+            color: var(--GAINSBORO);
             font-family: Lato, Helvetica, Roboto, sans-serif;
             text-align: center;
             border: 1em;
         }
 
-        .table 
-        {
+        .table {
             margin-left: auto;
             margin-right: auto;
         }
 
-        .button 
-        {
+        .button {
             border-radius: 4px;
             border: none;
             padding: 15px 30px;
@@ -50,27 +42,25 @@ const char INDEX_HTML[] PROGMEM = R"rawliteral(
             display: inline-block;
             font-size: 13px;
         }
-        .blue 
-        {
-            background-color: #40798C;
-            color: #241E4E;
-        }
-        .green 
-        {
-            background-color: #399E5A;
-            color: #241E4E;
-        }
-        .red
-        {
-            background-color: #CE6C47;
-            color: #241E4E;
+
+        .blue {
+            background-color: var(--TEAL_BLUE);
+            color: var(--RUSSIAN_VIOLETT);
         }
 
-        .text_field 
-        {
+        .green {
+            background-color: var(--GREEN_COLOR);
+            color: var(--RUSSIAN_VIOLETT);
+        }
+
+        .red {
+            background-color: var(--RED_COLOR);
+            color: var(--RUSSIAN_VIOLETT);
+        }
+
+        .text_field {
             border-radius: 4px;
-            border: none;
-            // color: black;
+            border: none; // color: black;
             text-align: center;
             text-decoration: none;
             display: inline-block;
@@ -79,20 +69,19 @@ const char INDEX_HTML[] PROGMEM = R"rawliteral(
         }
 
         /* set the opacity of placeholder */
-        ::placeholder 
-        {
+
+        ::placeholder {
             opacity: 1;
         }
 
-        :focus::placeholder 
-        {
+        :focus::placeholder {
             opacity: 0.1;
         }
     </style>
 </head>
 <script>
-    window.onload = function() 
-    {
+    window.onload = function() {
+
         // check if variable exists
         let ssid = "%ssid%";
         let password = "%password%";
@@ -106,119 +95,87 @@ const char INDEX_HTML[] PROGMEM = R"rawliteral(
         let sub_topic_3 = "%sub_topic_3%";
         let sleep_time_sec = "%sleep_time_sec%";
 
-        if (ssid) 
-        {
+        if (ssid) {
             document.getElementsByName("ssid")[0].placeholder = ssid;
             document.getElementsByName("ssid")[0].style.color = "black";
-        }
-        else 
-        {
+        } else {
             document.getElementsByName("ssid")[0].placeholder = "Your wifi SSID";
             document.getElementsByName("ssid")[0].style.color = "grey";
         }
-        if (password) 
-        {
+        if (password) {
             document.getElementsByName("password")[0].placeholder = password;
             document.getElementsByName("password")[0].style.color = "black";
-        }
-        else
-        {   document.getElementsByName("password")[0].placeholder = "Your wifi password";
+        } else {
+            document.getElementsByName("password")[0].placeholder = "Your wifi password";
             document.getElementsByName("password")[0].style.color = "grey";
         }
-        if (broker_ip) 
-        {
+        if (broker_ip) {
             document.getElementsByName("broker_ip")[0].placeholder = broker_ip;
             document.getElementsByName("broker_ip")[0].style.color = "black";
-        }
-        else
-        {   
+        } else {
             document.getElementsByName("broker_ip")[0].placeholder = "Your broker IP";
             document.getElementsByName("broker_ip")[0].style.color = "grey";
         }
-        if (thing_name) 
-        {
+        if (thing_name) {
             document.getElementsByName("thing_name")[0].placeholder = thing_name;
             document.getElementsByName("thing_name")[0].style.color = "black";
-        }
-        else
-        {   
+        } else {
             document.getElementsByName("thing_name")[0].placeholder = "Enter a thing name";
             document.getElementsByName("thing_name")[0].style.color = "grey";
         }
-        if (pub_topic_1) 
-        {
+        if (pub_topic_1) {
             document.getElementsByName("pub_topic_1")[0].placeholder = pub_topic_1;
             document.getElementsByName("pub_topic_1")[0].style.color = "black";
-        }
-        else
-        {   
+        } else {
             document.getElementsByName("pub_topic_1")[0].placeholder = "E.g.: broker/place/value1";
             document.getElementsByName("pub_topic_1")[0].style.color = "grey";
         }
-        if (pub_topic_2) 
-        {
-            document.getElementsByName("pub_topic_1")[0].placeholder = pub_topic_1;
-            document.getElementsByName("pub_topic_1")[0].style.color = "black";
-        }
-        else
-        {   
+        if (pub_topic_2) {
+            document.getElementsByName("pub_topic_2")[0].placeholder = pub_topic_2;
+            document.getElementsByName("pub_topic_2")[0].style.color = "black";
+        } else {
             document.getElementsByName("pub_topic_2")[0].placeholder = "E.g.: broker/place/value2";
             document.getElementsByName("pub_topic_2")[0].style.color = "grey";
         }
-        if (pub_topic_3) 
-        {
+        if (pub_topic_3) {
             document.getElementsByName("pub_topic_3")[0].placeholder = pub_topic_3;
             document.getElementsByName("pub_topic_3")[0].style.color = "black";
-        }
-        else
-        {   
+        } else {
             document.getElementsByName("pub_topic_3")[0].placeholder = "E.g.: broker/place/value3";
             document.getElementsByName("pub_topic_3")[0].style.color = "grey";
         }
-        if (sub_topic_1) 
-        {
+        if (sub_topic_1) {
             document.getElementsByName("sub_topic_1")[0].placeholder = sub_topic_1;
             document.getElementsByName("sub_topic_1")[0].style.color = "black";
-        }
-        else
-        {   
+        } else {
             document.getElementsByName("sub_topic_1")[0].placeholder = "E.g.: broker/place/value1";
             document.getElementsByName("sub_topic_1")[0].style.color = "grey";
         }
-        if (sub_topic_2) 
-        {
-            document.getElementsByName("sub_topic_1")[0].placeholder = sub_topic_1;
-            document.getElementsByName("sub_topic_1")[0].style.color = "black";
-        }
-        else
-        {   
+        if (sub_topic_2) {
+            document.getElementsByName("sub_topic_2")[0].placeholder = sub_topic_2;
+            document.getElementsByName("sub_topic_2")[0].style.color = "black";
+        } else {
             document.getElementsByName("sub_topic_2")[0].placeholder = "E.g.:  broker/place/value2";
             document.getElementsByName("sub_topic_2")[0].style.color = "grey";
         }
-        if (sub_topic_3) 
-        {
+        if (sub_topic_3) {
             document.getElementsByName("sub_topic_3")[0].placeholder = sub_topic_3;
             document.getElementsByName("sub_topic_3")[0].style.color = "black";
-        }
-        else
-        {   
+        } else {
             document.getElementsByName("sub_topic_3")[0].placeholder = "E.g.:  broker/place/value3";
             document.getElementsByName("sub_topic_3")[0].style.color = "grey";
         }
-        if (sleep_time_sec) 
-        {
+        if (sleep_time_sec) {
             document.getElementsByName("sleep_time_sec")[0].placeholder = sleep_time_sec;
             document.getElementsByName("sleep_time_sec")[0].style.color = "black";
-        }
-        else
-        {   
+        } else {
             document.getElementsByName("sleep_time_sec")[0].placeholder = "Sleep time in seconds.";
             document.getElementsByName("sleep_time_sec")[0].style.color = "grey";
         }
 
         console.log(ssid);
         console.log(password);
-        console.log(broker_ip); 
+        console.log(broker_ip);
         console.log(thing_name);
         console.log(pub_topic_1);
         console.log(pub_topic_2);
@@ -235,108 +192,109 @@ const char INDEX_HTML[] PROGMEM = R"rawliteral(
     <form id="Form2" onsubmit="return confirm('Are you sure? All saved files would be deleted.');" action='actionWipeData' method='post' target="hidden-form"></form>
     <form id="Form3" onsubmit="return confirm('Connection will be lost during reboot.');" action='actionRebootESP' method='post' target="hidden-form"></form>
     <input form="Form1" type="hidden" id="radio_state" value=%location_method%>
-        <table class="table">
-            <tr>
-                <td colspan=4>
-                    <h2>Thing setup</h2>
-                </td>
-                <td></td>
-            </tr>
-            <tr>
-                <td colspan=4>
-                    <h3>MQTT broker</h3>
-                </td>
-            </tr>
-            <tr>
-                <td style="text-align:left;">SSID:</td>
-                <td colspan=3>
-                    <input class="text_field" form="Form1" type="text" name="ssid" placeholder={{ssid}}>
-                </td>
-            </tr>
-            <tr>
-                <td style="text-align:left;">Password:</td>
-                <td colspan=3>
-                    <input class="text_field" form="Form1" type="text" name="password" placeholder={{password}}>
-                </td>
-            </tr>
-            <tr>
-                <td style="text-align:left;">Broker IP:</td>
-                <td colspan=3>
-                    <input class="text_field" form="Form1" type="text" name="broker_ip" placeholder={{broker_ip}}>
-                </td>        
-            </tr>
-            <tr>
-                    <td colspan=4>
-                    <h3>MQTT client</h3>
-                </td>
-            </tr>
-            <tr>
-                <td style="text-align:left;">Thing name:</td>
-                <td colspan=3>
-                    <input class="text_field" form="Form1" type="text" name="thing_name" placeholder={{thing_name}}>
-                </td>
-            </tr>
-            <tr>
-                <td style="text-align:left;">Pub topic 1:</td>
-                <td>
-                    <input class="text_field" form="Form1" type="text" name="pub_topic_1" placeholder={{pub_topic_1}}>
-                </td>
-            </tr>
-            <tr>
-                 <td style="text-align:left;">Pub topic 2:</td>
-                <td colspan=3>
-                    <input class="text_field" form="Form1" type="text" name="pub_topic_2" placeholder={{pub_topic_2}}>
-                </td>
-            </tr>
-            <tr>
-                <td style="text-align:left;">Pub topic 3:</td>
-                   <td colspan=3>
-                    <input class="text_field" form="Form1" type="text" name="pub_topic_3" placeholder={{pub_topic_3}}>
-                </td>
-            </tr>
-            <tr>
-                <td style="text-align:left;">Sub topic 1:</td>
-                <td colspan=3>
-                    <input class="text_field" form="Form1" type="text" name="sub_topic_1" placeholder={{sub_topic_1}}>
-                </td>
-            </tr>
-               <tr>
-                <td style="text-align:left;">Sub topic 2:</td>
-                <td colspan=3>
-                    <input class="text_field" form="Form1" type="text" name="sub_topic_2" placeholder=sub_topic_2>
-                </td>
-            </tr>
-            <tr>
-                <td style="text-align:left;">Sub topic 3:</td>
-                <td colspan=3>
-                    <input class="text_field" form="Form1" type="text" name="sub_topic_3" placeholder={{sub_topic_3}}>
-                </td>
-            </tr>
-            <tr>
-                <td colspan=4> </td>
-                <td> </td>
-            </tr>
-            <tr>
-                 <td colspan=4>
-                      <h3>Device settings</h3>
-                   </td>
-            </tr>
-            <tr>
-                  <td style="text-align:left;">Sleep time, s:</td>
-                <td colspan=3>
-                      <input class="text_field" form="Form1" type="text" name="sleep_time_sec" placeholder={{sleep_time_sec}}>
-                </td>
-            </tr>
-        </table>
-        <br>
-        <br>
-        <div>
-            <input type="submit" form="Form1" class="button green" formaction="/actionUpdateData" value="Save" id="save_button" name="save_button" />
-            <input type="submit" form="Form3" class="button green" formaction="/actionRebootESP32" value="Reboot" id="reboot_button" name="reboot_button" />
-            <input type="reset" form="Form1" class="button blue" value="Cancel" />
-            <input type="submit" form="Form2" class="button red" formaction="/actionWipeData" value="Wipe" id="wipe_button" name="wipe_button" />
-        </div>
+    <table class="table">
+        <tr>
+            <td colspan=4>
+                <h2>Thing setup</h2>
+            </td>
+            <td></td>
+        </tr>
+        <tr>
+            <td colspan=4>
+                <h3>MQTT broker</h3>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align:left;">SSID:</td>
+            <td colspan=3>
+                <input class="text_field" form="Form1" type="text" name="ssid" placeholder={{ssid}}>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align:left;">Password:</td>
+            <td colspan=3>
+                <input class="text_field" form="Form1" type="text" name="password" placeholder={{password}}>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align:left;">Broker IP:</td>
+            <td colspan=3>
+                <input class="text_field" form="Form1" type="text" name="broker_ip" placeholder={{broker_ip}}>
+            </td>
+        </tr>
+        <tr>
+            <td colspan=4>
+                <h3>MQTT client</h3>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align:left;">Thing name:</td>
+            <td colspan=3>
+                <input class="text_field" form="Form1" type="text" name="thing_name" placeholder={{thing_name}}>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align:left;">Pub topic 1:</td>
+            <td>
+                <input class="text_field" form="Form1" type="text" name="pub_topic_1" placeholder={{pub_topic_1}}>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align:left;">Pub topic 2:</td>
+            <td colspan=3>
+                <input class="text_field" form="Form1" type="text" name="pub_topic_2" placeholder={{pub_topic_2}}>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align:left;">Pub topic 3:</td>
+            <td colspan=3>
+                <input class="text_field" form="Form1" type="text" name="pub_topic_3" placeholder={{pub_topic_3}}>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align:left;">Sub topic 1:</td>
+            <td colspan=3>
+                <input class="text_field" form="Form1" type="text" name="sub_topic_1" placeholder={{sub_topic_1}}>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align:left;">Sub topic 2:</td>
+            <td colspan=3>
+                <input class="text_field" form="Form1" type="text" name="sub_topic_2" placeholder={{sub_topic_2}}>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align:left;">Sub topic 3:</td>
+            <td colspan=3>
+                <input class="text_field" form="Form1" type="text" name="sub_topic_3" placeholder={{sub_topic_3}}>
+            </td>
+        </tr>
+        <tr>
+            <td colspan=4> </td>
+            <td> </td>
+        </tr>
+        <tr>
+            <td colspan=4>
+                <h3>Device settings</h3>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align:left;">Sleep time, s:</td>
+            <td colspan=3>
+                <input class="text_field" form="Form1" type="text" name="sleep_time_sec" placeholder={{sleep_time_sec}}>
+            </td>
+        </tr>
+    </table>
+    <br>
+    <br>
+    <div>
+        <input type="submit" form="Form1" class="button green" formaction="/actionUpdateData" value="Save" id="save_button" name="save_button" />
+        <input type="submit" form="Form3" class="button green" formaction="/actionRebootESP32" value="Reboot" id="reboot_button" name="reboot_button" />
+        <input type="reset" form="Form1" class="button blue" value="Cancel" />
+        <input type="submit" form="Form2" class="button red" formaction="/actionWipeData" value="Wipe" id="wipe_button" name="wipe_button" />
+    </div>
 </body>
+
 </html>
 
 )rawliteral";
