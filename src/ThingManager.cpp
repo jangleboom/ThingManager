6a61379971531a8781cadb9021267242f2cd784f
Markus Hädrich <markus.haedrich@mail.de>
2023-02-22 22:25:23 +0100

    default OTA user name and pw set

--- src/ThingManager.cpp
+++ src/ThingManager.cpp
@@ -211,7 +211,7 @@ bool ThingManager::setupWiFi(AsyncWebServer* server)
         }
         delay(500);
         setServerCallbacks(server); 
-        AsyncElegantOTA.begin(server);  // Start ElegantOTA
+        AsyncElegantOTA.begin(server, OTA_USER, OTA_PW);   // Start ElegantOTA
         server->begin();  // Start WebInterface + OTA (http://LOCAL_IP/update)
         delay(500);
       }
